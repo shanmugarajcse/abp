68f8fa4f7f66edb0a633d68259cfa6353c5b5a8d
enisn <iletisim@enisnecipoglu.com>
2020-12-29 10:51:55 +0300

    Initial draft commit of dbmigrator

---
+++ modules/cms-kit/host/Volo.CmsKit.DbMigrator/CmsKitDbMigratorModule.cs
@@ -0,0 +1,18 @@
+﻿using Volo.Abp.Autofac;
+using Volo.Abp.BackgroundJobs;
+using Volo.Abp.Modularity;
+
+namespace Volo.CmsKit.DbMigrator
+{
+    [DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(CmsKitWebUnifiedModule)
+    )]
+    public class CmsKitDbMigratorModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpBackgroundJobOptions>(options => options.IsJobExecutionEnabled = false);
+        }
+    }
+}
---
+++ modules/cms-kit/host/Volo.CmsKit.DbMigrator/DbMigratorHostedService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Volo.CmsKit.DbMigrator
+{
+    public class DbMigratorHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+        public DbMigratorHostedService(IHostApplicationLifetime hostApplicationLifetime)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var application = AbpApplicationFactory.Create<CmsKitDbMigratorModule>(options =>
+            {
+                options.UseAutofac();
+                options.Services.AddLogging(c => c.AddSerilog());
+            }))
+            {
+                application.Initialize();
+
+                await application
+                    .ServiceProvider
+                    .GetRequiredService<CmsKitDbMigrationService>()
+                    .MigrateAsync();
+
+                application.Shutdown();
+
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
